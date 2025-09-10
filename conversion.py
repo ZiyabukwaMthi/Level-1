f376a3b07de5f0312a77f265a9d7c0900a24cb96
ZiyabukwaMthi <ziyamthi@gmail.com>
2025-09-10 13:54:45 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,17 @@
+# Declare variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert variables
+num_1 = int(num1)           # float to integer
+num_2 = float(num2)         # integer to float
+num_3 = str(num3)           # integer to string
+string_1 = int(string1)     # string to integer
+
+# Print variables on separate lines
+print(num_1)
+print(num_2)
+print(num_3)
+print(string_1)
