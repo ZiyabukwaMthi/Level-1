198247451d3abd98df3b45cfebf5f9e4742ab24d
ZiyabukwaMthi <ziyamthi@gmail.com>
2025-09-10 17:30:31 +0200

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,11 @@
+
+# Save the sentence as a string
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+# Replace "!" with spaces
+replaced_sentence = sentence.replace("!", " ")
+print(replaced_sentence)
+
+# Convert to uppercase
+uppercase_sentence = replaced_sentence.upper()
+print(uppercase_sentence)
